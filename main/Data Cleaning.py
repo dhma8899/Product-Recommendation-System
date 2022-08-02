065ddf7f622f809c477ce13653358962268dfb76
dhma8899 <dhma8899@colorado.edu>
2022-08-02 12:52:04 -0600

    On branch main 	new file:   Dataset/archive dataset2.zip 	new file:   Dataset/archive dataset2/online_retail_II.csv 	new file:   Dataset/archive dataset2/online_retail_II.xlsx 	new file:   Dataset/archive.zip 	new file:   Dataset/archive/Assignment-1_Data.csv 	new file:   Dataset/archive/Assignment-1_Data.xlsx 	new file:   main/.idea/inspectionProfiles/Project_Default.xml 	new file:   main/.idea/inspectionProfiles/profiles_settings.xml 	new file:   main/.idea/main.iml 	new file:   main/.idea/misc.xml 	new file:   main/.idea/modules.xml 	new file:   main/.idea/workspace.xml 	new file:   main/Data Cleaning.py 	new file:   main/Dataset1.csv 	new file:   main/Dataset2.csv 	new file:   main/Reference/New Text Document.txt 	new file:   main/dataframe1.csv 	new file:   main/dataframe2.csv 	new file:   main/sample1.csv 	new file:   main/sample2.csv 	new file:   main/transformation.py

---
+++ main/Data Cleaning.py
@@ -0,0 +1,59 @@
+'''
+Theory
+
+Step1:- Remove all rows in which country is not UK
+Step2:- Remove all the rows in which any relevent parameter is empty
+Step3:- Remove irrelevent columns Assignment-1_Data(Quantity, Price, CustomerId, Country)
+        online_retail_11.csv (StockCode, Quantity, Price, Customer, Country)
+'''
+
+import numpy
+import pandas as pd
+
+def country(df):
+    df.drop(df[df['Country'] != "United Kingdom"].index, inplace = True)
+    return df
+
+def emptyrows(df):
+    try:
+        data = df.dropna(subset=['Invoice', 'Description', 'InvoiceDate'])
+    except:
+        data = df.dropna(subset=['BillNo', 'Itemname', 'Date'])
+    return data
+
+def irrelevent(df):
+    try:
+        df = df.drop('CustomerID', axis=1)
+        df = df.drop('Quantity', axis=1)
+        df = df.drop('Price', axis=1)
+        df = df.drop('Country', axis=1)
+    except:
+        df = df.drop('StockCode', axis=1)
+        df = df.drop('Quantity', axis=1)
+        df = df.drop('Price', axis=1)
+        df = df.drop('Customer ID', axis=1)
+        df = df.drop('Country', axis=1)
+    return df
+
+if __name__ == "__main__":
+    File1 = "Dataset1.csv"
+    File2 = "Dataset2.csv"
+
+    df1 = pd.read_csv('Dataset1.csv', error_bad_lines=False, sep=';')
+    df2 = pd.read_csv('Dataset2.csv', error_bad_lines=False)
+
+
+
+    df1 = country(df1)
+    df2 = country(df2)
+
+
+    df1 = emptyrows(df1)
+    df2 = emptyrows(df2)
+
+    df1 = irrelevent(df1)
+    df2 = irrelevent(df2)
+
+
+    df1.to_csv('dataframe1.csv',index=False)
+    df2.to_csv('dataframe2.csv',index=False)
